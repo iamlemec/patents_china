3972c9cf1de399018d209e4135f7e96c74eea8e7
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-06-21 01:14:37 -0400

    firm match

--- firm_pats.py
+++ firm_pats.py
@@ -17,13 +17,13 @@ tax_df = tax_df.rename(columns={args.idcol: 'id', args.namecol: 'name'})
 tax_df = tax_df.drop_duplicates(subset='id')
 
 # load patent data
-with sqlite3.connect('/media/Liquid/work/cleancomp/code/store/patents_cn.db') as con:
-    pat_df = pd.read_sql('select patnum,appname,appdate from patent', con)
+with sqlite3.connect('/home/doug/data/patents_china/store/patents.db') as con:
+    pat_df = pd.read_sql('select appnum,appname,appdate from patent', con)
 
 # split names
-pat1_df = pd.DataFrame(list(chain(*[[(s['patnum'], s['appdate'], x) for x in s['appname'].split(';')] for _, s in pat_df.iterrows()])), columns=['patnum', 'appdate', 'appname'])
+pat1_df = pd.DataFrame(list(chain(*[[(s['appnum'], s['appdate'], x) for x in s['appname'].split(';')] for _, s in pat_df.iterrows()])), columns=['appnum', 'appdate', 'appname'])
 
 # merge datasets
-merged = pd.merge(pat1_df[['patnum', 'appdate', 'appname']], tax_df[['id', 'name']], left_on='appname', right_on='name', how='left')[['patnum', 'appdate', 'id']]
+merged = pd.merge(pat1_df[['appnum', 'appdate', 'appname']], tax_df[['id', 'name']], left_on='appname', right_on='name', how='left')[['appnum', 'appdate', 'id']]
 
 merged.to_csv(args.output, index=False)
