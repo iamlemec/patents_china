d00d9cabace183cbc1e2c015f5d486244ffe7f4c
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-08-01 00:51:02 -0500

    allow for cross-corpus comparison in similarity

--- make_jsonl.py
+++ make_jsonl.py
@@ -1,28 +1,44 @@
 import json
-import argparse
 import pandas as pd
+from glob import glob
 
-# parse input arguments
-parser = argparse.ArgumentParser(description='China patent parser.')
-parser.add_argument('inpath', type=str, help='csv file to parse')
-parser.add_argument('outpath', type=str, help='jsonl file to store to')
-parser.add_argument('--chunk', type=int, default=8192, help='chunk size')
-parser.add_argument('--limit', type=int, default=None, help='only parse n patents')
-args = parser.parse_args()
+# globals
+columns_utility = ['app_no', 'app_date', 'title', 'claims', 'abstract']
+columns_invention = ['appnum', 'appdate', 'title', 'claims', 'abstract']
+columns_output = ['appnum', 'appdate', 'title', 'claims', 'abstract']
 
-# write to jsonl
-with open(args.outpath, 'w') as fid:
-    for batch in pd.read_csv(
-        args.inpath, chunksize=args.chunk, nrows=args.limit,
-        usecols=['patnum', 'title', 'claims', 'abstract']
-    ):
-        for _, row in batch.iterrows():
-            dat = {
-                'patnum': row['patnum'],
-                'text': f"{row['title']}\n{row['claims']}\n{row['abstract']}",
-            }
-            json.dump(dat, fid, ensure_ascii=False)
-            fid.write('\n')
+rename_utility = {'app_no': 'appnum', 'app_date': 'appdate'}
+rename_invention = {}
+
+# combine utility CSV files
+def merge_patents(indir, outpath, mode):
+    if mode == 'utility':
+        columns = columns_utility
+        rename = rename_utility
+    elif mode == 'invention':
+        columns = columns_invention
+        rename = rename_invention
+
+    with open(outpath, 'w') as fid:
+        fid.write(','.join(columns_output)+'\n')
+        for path in sorted(glob(f'{indir}/*.csv')):
+            print(path)
+            data = pd.read_csv(path, usecols=columns, encoding_errors='ignore')
+            data = data.rename(rename, axis=1)[columns_output]
+            data['appdate'] = pd.to_datetime(data['appdate'])
+            data.to_csv(fid, header=False, index=False)
+
+# write to json
+def make_jsonl(inpath, outpath, chunk=8192, limit=None):
+    with open(outpath, 'w') as fid:
+        for batch in pd.read_csv(inpath, chunksize=chunk, nrows=limit):
+            for _, row in batch.iterrows():
+                dat = {
+                    'appnum': row['appnum'],
+                    'text': f"{row['title']}\n{row['claims']}\n{row['abstract']}",
+                }
+                json.dump(dat, fid, ensure_ascii=False)
+                fid.write('\n')
 
 # load with ziggy (4 hours on A6000)
 # emb = ziggy.LlamaCppEmbedding('bge-small-zh-v1.5-f16.gguf')
--- similarity.py
+++ similarity.py
@@ -5,15 +5,27 @@ import pandas as pd
 from ziggy import TorchVectorIndex
 from ziggy.utils import batch_indices
 
+# demean and renormalize vectors
+def demean_inplace(x):
+    x -= x.mean(dim=0)[None,:]
+    x /= x.square().sum(dim=1)[:,None]
+
+# load ziggy TorchVectorIndex directly or from TextDatabase
+def load_database(path):
+    data = torch.load(path)
+    if 'index' in data:
+        data = data['index']
+    return TorchVectorIndex.load(data)
+
+# merge patent metadata with ziggy database
 def merge_patents(
-    path_vecs='data/tables/patents.torch',
-    path_meta='data/tables/patents.csv',
-    path_pats='data/simil/pats.csv'
+    path_vecs, # ziggy database
+    path_meta, # metadata csv
+    path_pats, # output csv
 ):
     # load vector index
     print('Loading vector index')
-    data = torch.load(path_vecs)
-    index = TorchVectorIndex.load(data)
+    index = load_database(path_vecs)
 
     # load metadata csv
     print('Loading patent metadata')
@@ -29,21 +41,27 @@ def merge_patents(
     pats.to_csv(path_pats, index=False)
 
 def similarity_topk(
-    path_vecs='data/tables/patents.torch',
-    path_pats='data/simil/pats.csv',
-    path_sims='data/simil/topk.torch',
+    path_vecs, # ziggy database
+    path_pats, # patent metadata csv (for comparison!)
+    path_sims, # output torch file
+    path_vecs1=None, # comparison ziggy database
     topk=100, batch_size=256, max_rows=None, demean=False
 ):
     # load vector index
     print('Loading vector index')
-    data = torch.load(path_vecs)
-    index = TorchVectorIndex.load(data)
+    index = load_database(path_vecs)
     n_pats = len(index)
+
+    # load comparison vector index
+    if path_vecs1 is not None:
+        print('Loading comparison vector index')
+        index1 = load_database(path_vecs1)
 
     # demean vectors is requested
     if demean:
-        index.values.data -= index.values.data.mean(dim=0)[None,:]
-        index.values.data /= index.values.data.square().sum(dim=1)[:,None]
+        demean_inplace(index.values.data)
+        if index1 is not index:
+            demean_inplace(index1.values.data)
 
     # limit rows if requested
     if max_rows is not None:
@@ -66,8 +84,8 @@ def similarity_topk(
         print(f'{i1} → {i2}')
 
         # compute similarities for batch
-        vecs = index.values.data[i1:i2]
-        sims = index.similarity(vecs)
+        vecs = index.values.data[i1:i2] # [B, D]
+        sims = index.similarity(vecs) # [B, N1]
 
         # compute top sims for before
         before = days[None, :] < days[i1:i2, None]
@@ -79,24 +97,35 @@ def similarity_topk(
         simt[i1:i2] = topb.values
 
     # save to disk
-    torch.save({'top_idx': idxt, 'top_sim': simt}, path_sims)
+    torch.save({
+        'top_idx': idxt,
+        'top_sim': simt,
+    }, path_sims)
 
 def similarity_mean(
-    path_vecs='data/tables/patents.torch',
-    path_pats='data/simil/pats.csv',
-    path_sims='data/simil/mean.torch',
+    path_vecs, # ziggy database
+    path_pats, # patent metadata csv (for comparison!)
+    path_sims, # output torch file
+    path_vecs1=None, # comparison ziggy database
     batch_size=64, max_rows=None, demean=False,
 ):
     # load vector index
-    print('Loading vector index')
-    data = torch.load(path_vecs)
-    index = TorchVectorIndex.load(data)
+    print('Loading base vector index')
+    index = load_database(path_vecs)
     n_pats = len(index)
+
+    # load comparison vector index
+    if path_vecs1 is not None:
+        print('Loading comparison vector index')
+        index1 = load_database(path_vecs1)
+    else:
+        index1 = index
 
     # demean vectors is requested
     if demean:
-        index.values.data -= index.values.data.mean(dim=0)[None,:]
-        index.values.data /= index.values.data.square().sum(dim=1)[:,None]
+        demean_inplace(index.values.data)
+        if index1 is not index:
+            demean_inplace(index1.values.data)
 
     # limit rows if requested
     if max_rows is not None:
@@ -124,8 +153,8 @@ def similarity_mean(
         n_batch = i2 - i1
 
         # compute similarities for batch
-        vecs = index.values.data[i1:i2]
-        sims = index.similarity(vecs)
+        vecs = index.values.data[i1:i2] # [B, D]
+        sims = index1.similarity(vecs) # [B, N1]
 
         # generate offsets
         batch_vec = torch.arange(n_batch, device='cuda')
@@ -137,6 +166,8 @@ def similarity_mean(
 
     # save to disk
     torch.save({
-        'year_sim': avgt, 'year_count': c_years,
-        'year_min': year_min, 'year_max': year_max,
+        'year_sim'  : avgt    ,
+        'year_count': c_years ,
+        'year_min'  : year_min,
+        'year_max'  : year_max,
     }, path_sims)
